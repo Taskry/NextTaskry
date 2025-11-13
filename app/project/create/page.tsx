54828bef06cadff6803cef5cb73fc774d4b3b4dd
unknown <nohf2002@gmail.com>
2025-11-13 16:38:27 +0900

    [feat] 프로젝트 대시보드 및 관리 페이지 UI 구상

---
+++ app/project/create/page.tsx
@@ -0,0 +1,10 @@
+import ProjectForm from "@/app/components/project/ProjectForm";
+    
+export default async function CreateProject() {
+    // id를 통해서 프로젝트 API 요청 작성
+    return (
+    <div>
+        <ProjectForm />
+    </div>
+    );
+}
