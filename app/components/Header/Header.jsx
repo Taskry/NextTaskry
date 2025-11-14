9d37d9f193e4e8f7290e54ccf9dfa6534ff7ff81
Won Chan LEE <gxpk2978@naver.com>
2025-11-14 22:17:22 +0900

    [feat/modal] : user 정보 모달 및 toast 추가

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -3,25 +3,45 @@
 import Link from "next/link";
 import { Icon } from "../Icon/Icon";
 import { useTheme } from "next-themes";
+import { useState } from "react";
+import ProfileModal from "@/app/login/components/ProfileModal";
+import { useSession } from "next-auth/react";
+import { showToast } from "@/lib/toast";
+
 
 export function Header() {
   const { theme, setTheme } = useTheme();
+  const [open, setOpen] = useState(false);
+  const { data: session } = useSession();
+
+
+  if(session){
+    console.log(session.user)
+  }
+
+  let handleLoginModal = () => {
+    if(!session) {
+      showToast("로그인이 필요합니다.", "alert")
+      return
+    }
+    setOpen(true)
+  }
+
+
 
   return (
+    <>
     <header className="w-full h-14 border-b border-gray-200 dark:border-gray-700 flex items-center justify-between px-6 bg-white dark:bg-black">
       
-      {/* 왼쪽: 로고 */}
       <Link href="/" className="flex items-center gap-2">
         <Icon type="board" size={22} className="text-main-500 dark:text-main-300" />
         <span className="font-bold text-main-600 dark:text-white text-lg">Taskry</span>
       </Link>
 
-      {/* 오른쪽: 아이콘 영역 */}
       <div className="flex items-center gap-4">
 
-        {/* ⭐ 팀원이 만든 '유저 아이콘' (프로필 버튼) */}
-        <Link
-          href="/user/profile"
+        <button 
+          onClick={handleLoginModal}
           className="w-10 h-10 rounded-full border border-gray-300 dark:border-gray-600 flex items-center justify-center hover:border-main-500 transition"
         >
           <Icon
@@ -29,9 +49,8 @@ export function Header() {
             size={20}
             className="text-gray-600 dark:text-gray-300"
           />
-        </Link>
+        </button>
 
-        {/* ⭐ 테마 토글 버튼 */}
         <button
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           className="w-10 h-10 rounded-full border border-gray-300 dark:border-gray-600 flex items-center justify-center hover:border-main-500 transition"
@@ -44,5 +63,11 @@ export function Header() {
         </button>
       </div>
     </header>
+    <ProfileModal
+      isOpen={open}
+      onClose={() => setOpen(false)}
+      user={session?.user ?? null}
+    />
+    </>
   );
 }
