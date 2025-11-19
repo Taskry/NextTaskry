d02ef7d451814c6d321c44597aa44e6e40a0f128
김민중 <f1minn2r@naver.com>
2025-11-19 17:15:51 +0900

    [feat/notice] 환경 변수 설정 주석 처리, supabase import로 변경

--- app/api/announcement/route.ts
+++ app/api/announcement/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
+import { supabase } from "@/lib/supabase";
 
-const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!
-);
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// );
 
 // ------------------------------------------------------
 // 공지사항 조회
