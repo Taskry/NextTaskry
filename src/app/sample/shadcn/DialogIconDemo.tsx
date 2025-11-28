5618dd511def277b0ab4cb665d3cc6841b3bbb03
unknown <nohf2002@gmail.com>
2025-11-28 20:17:01 +0900

    [feat] shadcn 샘플 컴포넌트 추가

--- src/app/sample/shadcn/DialogIconDemo.tsx
+++ src/app/sample/shadcn/DialogIconDemo.tsx
@@ -11,14 +11,17 @@ import {
 } from "@/components/ui/shadcn/Dialog"
 import React, { useState } from "react";
 
-interface DeleteDialogProps {
-  onClick: () => void;
+interface DialogIconDemoProps {
+  title: string;
+  description?: string;
+  onClick?: () => void;
 }
 
-export function DeleteDialog({ onClick }: DeleteDialogProps) {
+export function DialogIconDemo({ title, description, onClick }: DialogIconDemoProps) {
   const [open, setOpen] = useState(false);
  const handleDelete = (e: React.MouseEvent) => {
-    onClick();
+    // e.preventDefault(); 
+    onClick?.();
     setOpen(false);
   };
 
@@ -42,28 +45,21 @@ export function DeleteDialog({ onClick }: DeleteDialogProps) {
           />
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>프로젝트를 삭제하시겠습니까?</DialogTitle>
-            <DialogDescription>
-              삭제한 프로젝트는 다시 되돌릴 수 없습니다.
-              프로젝트 관련 모든 데이터가 삭제됩니다.
+          <DialogHeader className="flex justify-self-center">
+            <DialogTitle className="text-center">{title}</DialogTitle>
+            <DialogDescription className="text-center">
+              {description}
             </DialogDescription>
           </DialogHeader>
-          <DialogFooter>
-            <Button 
-              variant="list" 
-              className="hover:bg-main-200/40"
-              onClick={handleDelete}
-            >
-                삭제
-            </Button>
-              
+          <DialogFooter className="flex justify-self-center">
             <DialogClose asChild>
               <Button 
                 variant="list" 
-                className="hover:bg-main-200/40">취소
+                className="bg-red-100! text-white">확인
               </Button>
             </DialogClose>
+              
+            
           </DialogFooter>
         </DialogContent>
       </form>
